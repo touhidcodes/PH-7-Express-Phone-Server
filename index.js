06cff67433b82d5ac1a2504654361eda16fe1a9f
Touhidur Zaman <touhidcodes@gmail.com>
2023-04-18 22:27:11 +0600

    installed-middleware-cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const phones = require("./phones.json");
+const cors = require("cors");
 const app = express();
 const port = 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
 	res.send("My Phones information coming soon...");
